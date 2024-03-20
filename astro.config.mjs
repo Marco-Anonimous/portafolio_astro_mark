dc28b7062f5af8da17169d39d715d68001d21b7d
Marco Antonio FM <luisfate10@gmail.com>
2024-03-20 13:18:53 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
+
 
 import mdx from "@astrojs/mdx";
 
@@ -9,5 +9,5 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "server",
-  adapter: netlify()
+
 });
--- src/pages/posts/Post.md
+++ src/pages/posts/Post.md
@@ -4,7 +4,7 @@ pubDate: 2022-07-01
 description: 'Este es la primera publicación de mi nuevo blog de Astro.'
 author: 'Alumno de Astro'
 image:
-    url: 'https://docs.astro.build/assets/full-logo-light.png'
+    
     alt: 'El logotipo completo de Astro.'
 tags: ["astro", "bloguear", "aprender en público"]
 layout: "./MarkdownPostLayout.astro"
