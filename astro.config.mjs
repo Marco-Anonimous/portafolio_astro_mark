fe5f3aa1f57ff16d16f3e53abc3ff21b261728eb
Marco Antonio FM * <162050547+Marco-Anonimous@users.noreply.github.com>
2024-03-20 13:44:32 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import netlify from "@astrojs/netlify";
 
 import mdx from "@astrojs/mdx";
 
